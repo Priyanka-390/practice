296724e8e7e148b5e0bf80a8febfc0cbe7a0df84
Priyanka-390 <psr82220@gmail.com>
2024-03-21 16:17:21 +0530

    alignments

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -4,11 +4,13 @@ import Link from 'next/link'
 
 const About = () => {
   return (
-    <div className='bg-black'>
-          <p className='text-white text-2xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, vel.</p>
-          <p className='text-white text-2xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, vel.</p>
-      <p className='text-white text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptas similique consectetur quisquam autem blanditiis ducimus nulla quas sit cumque.</p>
-      <Link href="./homepage"><Button text="home"/></Link>
+    <div className='bg-black min-h-screen flex justify-center flex-col items-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <p className='text-white text-center text-2xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, vel.</p>
+            <p className='text-white text-center text-2xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, vel.</p>
+                  <p className='text-white text-center text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptas similique consectetur quisquam autem blanditiis ducimus nulla quas sit cumque.</p>
+                  <Link href="/"><Button text="home"/></Link>
+          </div>
     </div>
   )
 }
